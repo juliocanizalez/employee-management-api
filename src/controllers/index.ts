3214740c5a896ebb55e93355aea634a464add94c
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-22 11:06:07 -0600

    Add controllers barrel file

---
+++ src/controllers/index.ts
@@ -0,0 +1,12 @@
+export {
+  getDepartmentController,
+  getDepartmentsController,
+  saveDepartmentController,
+} from "./department.controller";
+
+export {
+  getEmployeeController,
+  getEmployeesController,
+  saveEmployeeController,
+  updateEmployeeController,
+} from "./employee.controller";
