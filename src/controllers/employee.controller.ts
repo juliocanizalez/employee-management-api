6a8eea4791bc905983fc6795087acf2beb0b6298
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-22 11:05:35 -0600

    Add employee controller

---
+++ src/controllers/employee.controller.ts
@@ -0,0 +1,73 @@
+import { Request, Response } from "express";
+import {
+  getEmployee,
+  getEmployees,
+  saveEmployee,
+  updateEmployee,
+} from "@services";
+
+export const getEmployeeController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const employee = await getEmployee(id);
+
+    res.status(200).json(employee);
+  } catch (error) {
+    res.status(404).json({ message: "Employee not found", details: error });
+  }
+};
+
+export const getEmployeesController = async (req: Request, res: Response) => {
+  try {
+    const employees = await getEmployees();
+
+    res.status(200).json(employees);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", details: error });
+  }
+};
+
+export const saveEmployeeController = async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName, hireDate, phone, address, departmentId } =
+      req.body;
+
+    const employee = await saveEmployee({
+      firstName,
+      lastName,
+      hireDate,
+      phone,
+      address,
+      departmentId,
+    });
+
+    res.status(201).json(employee);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", details: error });
+  }
+};
+
+export const updateEmployeeController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, hireDate, phone, address, departmentId } =
+      req.body;
+
+    const employee = await updateEmployee({
+      id,
+      firstName,
+      lastName,
+      hireDate,
+      phone,
+      address,
+      departmentId,
+    });
+
+    res.status(200).json(employee);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", details: error });
+  }
+};
