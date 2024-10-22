aa14c0981844c6f781260c8d256b90ec1e72d6f9
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-22 09:59:53 -0600

    Add department service

---
+++ src/services/department.service.ts
@@ -0,0 +1,47 @@
+import prisma from "@config";
+import { Department } from "types/department.type";
+
+export const getDepartment = async (id: string) => {
+  const department = await prisma.department.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      _count: {
+        select: {
+          Employee: true,
+        },
+      },
+    },
+  });
+
+  return department;
+};
+
+export const getDepartments = async () => {
+  const departments = await prisma.department.findMany({
+    select: {
+      id: true,
+      name: true,
+      _count: {
+        select: {
+          Employee: true,
+        },
+      },
+    },
+  });
+
+  return departments;
+};
+
+export const saveDepartment = async (department: Department) => {
+  const newDepartment = await prisma.department.create({
+    data: {
+      name: department.name,
+    },
+  });
+
+  return newDepartment;
+};
