a5d118faee7e15efbd1ee25b9e322968edeb7b56
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-22 11:05:02 -0600

    Add services barrel file

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+export {
+  getDepartment,
+  getDepartments,
+  saveDepartment,
+} from "./department.service";
+export {
+  getEmployee,
+  getEmployees,
+  saveEmployee,
+  updateEmployee,
+} from "./employee.service";
