ebca8f021a41d0cf5448e021e00575534aa1e65d
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-27 21:38:21 -0600

    update routes barrel file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import departmentRoutes from "./department.routes";
 import employeeRoutes from "./employee.routes";
+import employeeDepartmentRoutes from "./employee-department.routes";
 
-export { departmentRoutes, employeeRoutes };
+export { departmentRoutes, employeeRoutes, employeeDepartmentRoutes };
