fb57391409789d4b0e2b2352f3f6cc1840295a40
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-27 21:11:36 -0600

    Add employee department service

---
+++ src/services/employee-department.service.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@config";
+import { EmployeeDepartment } from "types/employee-department.type";
+
+export const getEmployeeDepartment = async (id: string) => {
+  const employeeDepartment = await prisma.employeeDepartmentHistory.findMany({
+    select: {
+      department: {
+        select: {
+          name: true,
+        },
+      },
+      createdAt: true,
+    },
+    where: {
+      employeeId: id,
+    },
+  });
+
+  return employeeDepartment;
+};
+
+export const saveEmployeeDepartment = async (data: EmployeeDepartment) => {
+  const { employeeId, departmentId } = data;
+
+  const employeeDepartment = await prisma.employeeDepartmentHistory.create({
+    data: {
+      employeeId,
+      departmentId,
+    },
+  });
+
+  return employeeDepartment;
+};
